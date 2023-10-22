6dd5e08fde30c06ac5bac77dd10cfdce92c02ce4
daniel.chidebere <daniel.chidebere@konga.com>
2023-10-22 21:07:21 +0100

    connect(database-mongodb

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,9 @@
-export const createNewUser =  (req, res) => {
-        res.send('Hello world')
+export const createNewUser = (req, res) => {
+        try {
+                const { first_name, last_name, email, password } = req.body
+                console.log(first_name, last_name, email, password)
+        } catch (error) {
+
+        }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,33 @@
 import express from 'express'
 import route from './api.js'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+
+// config
+dotenv.config()
 // Create Application
 const app = express()
-
+app.use(express.json())
+// middleware
 app.use('/api', route)
 
-app.listen(5000, () => {
-    console.log(`Your Port is hosted at localhost:${5000}`)
+
+// connect to Database 
+
+mongoose.connect(process.env.MONGOOSE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'the database is not connected'))
+
+db.once('open', () => {
+    console.log('You are connected to database')
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Your Port is hosted at localhost:${process.env.PORT}`)
 })
